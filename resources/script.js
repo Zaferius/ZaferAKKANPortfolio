f9b0737f14be1e286baf5aaff80a2c9b3e1e48c6
Zaferius <akkanzafer@gmail.com>
2024-09-22 19:08:36 +0300

    nav buttons disabled glow nav buttons active text added Info removed Contact hyperlinks added

--- resources/script.js
+++ resources/script.js
@@ -4,92 +4,97 @@ var navButtons = document.getElementsByClassName("navButton");
 var navButtonsTexts = [];
 
 function chooseSection(button) {
-  // navButton'ların innerText'lerini bir diziye ekleyelim
-  for (var i = 0; i < navButtons.length; i++) {
-    navButtonsTexts.push(navButtons[i].innerText);
-  }
-
-  for (var i = 0; i < navButtons.length; i++) {
-    // Her section için döngü oluşturuyoruz
-    navButtons[i].disabled = false;
-    navButtons[i].innerText = navButtonsTexts[i];
-  }
-
-  button.disabled = true;
-  button.innerText = "•";
-
-  var sections = document.getElementsByClassName("msection"); // Tüm msection sınıflarını seçiyoruz
-  for (var i = 0; i < sections.length; i++) {
-    // Her section için döngü oluşturuyoruz
-    sections[i].classList.remove("active");
-    sections[i].classList.add("hidden");
-  }
-
-  if (button.classList.contains("homeButton")) {
-    var section = document.querySelector(".home-section");
-    if (section) {
-      section.classList.remove("hidden");
-      section.classList.add("active");
+    // navButton'ların innerText'lerini bir diziye ekleyelim
+    for (var i = 0; i < navButtons.length; i++) {
+        navButtonsTexts.push(navButtons[i].innerText);
     }
-  }
 
-  if (button.classList.contains("projectsButton")) {
-    var section = document.querySelector(".projects-section");
-    if (section) {
-      section.classList.remove("hidden");
-      section.classList.add("active");
+    for (var i = 0; i < navButtons.length; i++) {
+        // Her section için döngü oluşturuyoruz
+        navButtons[i].disabled = false;
+        navButtons[i].innerText = navButtonsTexts[i];
     }
-  }
 
-  if (button.classList.contains("infoButton")) {
-    var section = document.querySelector(".info-section");
-    if (section) {
-      section.classList.remove("hidden");
-      section.classList.add("active");
-    }
-  }
+    button.disabled = true;
+    button.innerText = "•" + " " + button.innerText;
 
-  if (button.classList.contains("contactButton")) {
-    var section = document.querySelector(".contact-section");
-    if (section) {
-      section.classList.remove("hidden");
-      section.classList.add("active");
+    var sections = document.getElementsByClassName("msection"); // Tüm msection sınıflarını seçiyoruz
+    for (var i = 0; i < sections.length; i++) {
+        // Her section için döngü oluşturuyoruz
+        sections[i].classList.remove("active");
+        sections[i].classList.add("hidden");
     }
-  }
+
+    if (button.classList.contains("homeButton")) {
+        var section = document.querySelector(".home-section");
+        if (section) {
+            section.classList.remove("hidden");
+            section.classList.add("active");
+        }
+    }
+
+    if (button.classList.contains("projectsButton")) {
+        var section = document.querySelector(".projects-section");
+        if (section) {
+            section.classList.remove("hidden");
+            section.classList.add("active");
+        }
+    }
+
+    if (button.classList.contains("infoButton")) {
+        var section = document.querySelector(".info-section");
+        if (section) {
+            section.classList.remove("hidden");
+            section.classList.add("active");
+        }
+    }
+
+    if (button.classList.contains("contactButton")) {
+        var section = document.querySelector(".contact-section");
+        if (section) {
+            section.classList.remove("hidden");
+            section.classList.add("active");
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var homeButton = document.getElementsByClassName("projectsButton")[0]; // İlk homeButton öğesini al
-  if (homeButton) {
-    chooseSection(homeButton); // Buton var ise fonksiyonu çağır
-  }
+    var button = document.getElementsByClassName("projectsButton")[0]; // İlk homeButton öğesini al
+    if (button) {
+        chooseSection(button); // Buton var ise fonksiyonu çağır
+    }
 
-  const gallery = document.getElementById("gallery");
-  const prevButton = document.getElementById("prev-button");
-  const nextButton = document.getElementById("next-button");
+    const buttons = document.querySelectorAll(".navButton");
+    const footer = document.getElementById("footer");
+    const border = document.getElementById("main-frame");
 
-  const images = ["images/hellevatorGif.gif", "images/hellevatorGif.gif"];
-  let currentIndex = 0;
+    border.style.opacity = "0";
+    footer.style.opacity = "0";
 
-  prevButton.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateImage();
-  });
-
-  nextButton.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateImage(); // Bu çalışmazsa, butonun doğru şekilde seçilemediği anlamına gelir
-  });
-
-  function updateImage() {
-    var img = gallery.querySelector("#current-image");
-    img.classList.add("fade-out");
     setTimeout(() => {
-      img.src = images[currentIndex];
-      img.alt = images[currentIndex];
-      img.classList.remove("fade-out");
-    }, 500);
-  }
+        border.style.opacity = "1";
+        footer.style.opacity = "1";
+        border.style.animation = "fadeIn 3s ease forwards";
+        footer.style.animation = "fadeIn 3s ease forwards";
+    }, 1000);
+
+    // Glow fonksiyonu (border elementine glow efekti ekleme)
+    function applyGlowEffect() {
+        border.style.animation = "none";
+
+        setTimeout(() => {
+            border.style.animation = "glowEffect 0.5s ease forwards";
+        }, 1);
+
+        border.addEventListener("animationend", function () {
+            border.style.animation = "";
+        });
+    }
+
+    // Her buton için tıklama olayını dinleyen bir döngü
+    buttons.forEach((button) => {
+        button.addEventListener("click", applyGlowEffect);
+    });
 });
 
 //HOME
